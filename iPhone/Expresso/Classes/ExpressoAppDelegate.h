40ffa2916ee3e850dace37f3d9d47f9547c96de7
David Cortés <david.cortes.18@gmail.com>
2010-11-18 18:15:56 +0100

    Afegides carpetes de iPhone, Local i Global amb un projecte net d'iPhone

---
+++ iPhone/Expresso/Classes/ExpressoAppDelegate.h
@@ -0,0 +1,31 @@
+//
+//  ExpressoAppDelegate.h
+//  Expresso
+//
+//  Created by David Cortés on 18/11/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface ExpressoAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+    
+@private
+    NSManagedObjectContext *managedObjectContext_;
+    NSManagedObjectModel *managedObjectModel_;
+    NSPersistentStoreCoordinator *persistentStoreCoordinator_;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSString *)applicationDocumentsDirectory;
+
+@end
+
---
+++ iPhone/Expresso/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Expresso
+//
+//  Created by David Cortés on 18/11/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
