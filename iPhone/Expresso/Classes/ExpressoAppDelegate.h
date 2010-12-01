f2a85b85d57c2e749f84f7bb4bb7d852a258f46b
Arol <arolet@gmail.com>
2010-12-01 18:09:40 +0100

    CartaXMLParser

--- iPhone/Expresso/Classes/ExpressoAppDelegate.h
+++ iPhone/Expresso/Classes/ExpressoAppDelegate.h
@@ -1,47 +1,1 @@
-//
-//  ExpressoAppDelegate.h
-//  Expresso
-//
-//  Created by David Cortés on 18/11/10.
-//  Copyright __MyCompanyName__ 2010. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
-#import <CoreData/CoreData.h>
-
-@interface ExpressoAppDelegate : NSObject <UIApplicationDelegate> {
-    
-    UIWindow *window;
-    
-@private
-    NSManagedObjectContext *managedObjectContext_;
-    NSManagedObjectModel *managedObjectModel_;
-    NSPersistentStoreCoordinator *persistentStoreCoordinator_;
-	UITextField *nombre;
-	UITextField *contrasena;
-	UIButton *inicia;
-	NSMutableData *responseData;
-}
-
-@property (nonatomic, retain) IBOutlet UIWindow *window;
-
-@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
-@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
-@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
-@property (nonatomic, retain) IBOutlet UITextField *nombre;
-@property (nonatomic, retain) IBOutlet UITextField *contrasena;
-@property (nonatomic, retain) IBOutlet UITextField *inicia;
-
-- (NSString *)applicationDocumentsDirectory;
-
--(IBAction) iniciaSessio;
-
-#pragma mark -
-#pragma mark Delegat del NSURLConnection
-
-- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data;
-- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error;
-
-@end
-
+////  ExpressoAppDelegate.h//  Expresso////  Created by David Cortés on 18/11/10.//  Copyright __MyCompanyName__ 2010. All rights reserved.//#import <UIKit/UIKit.h>#import <CoreData/CoreData.h>@interface ExpressoAppDelegate : NSObject <UIApplicationDelegate> {        UIWindow *window;    @private    NSManagedObjectContext *managedObjectContext_;    NSManagedObjectModel *managedObjectModel_;    NSPersistentStoreCoordinator *persistentStoreCoordinator_;	UITextField *nombre;	UITextField *contrasena;	UIButton *inicia;	NSMutableData *responseData;@public	NSDictionary *carta;}@property (nonatomic, retain) IBOutlet UIWindow *window;@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;@property (nonatomic, retain) IBOutlet UITextField *nombre;@property (nonatomic, retain) IBOutlet UITextField *contrasena;@property (nonatomic, retain) IBOutlet UITextField *inicia;- (NSString *)applicationDocumentsDirectory;-(IBAction) iniciaSessio;#pragma mark -#pragma mark Delegat del NSURLConnection- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data;- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error;@end
