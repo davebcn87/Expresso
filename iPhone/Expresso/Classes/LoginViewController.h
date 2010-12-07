155089210920db4a79b19ed6296cb22dbc17440a
Arol <arolet@gmail.com>
2010-12-07 23:20:53 +0100

    Canvis al FirstView i al AppDelegate
    He ficat uns canvis de vista a la firstview i he posat uns camps publics a l'appDelegate referits a l'usuari i la contrasenya que la necessitarem a lo llarg de tota l'acció. Aquests els actualitzo desde la pantalla de Login al fer aquest correctament.
    
    A més també he afegit un botó de log out visible desde tota la navegació del programa. En aquest setnit només faltaria tirar la navegació cap a la RootView i anar ficant el reset de tots els camps del programa.

--- iPhone/Expresso/Classes/LoginViewController.h
+++ iPhone/Expresso/Classes/LoginViewController.h
@@ -1,32 +1,1 @@
-//
-//  LoginViewController.h
-//  Expresso
-//
-//  Created by David Cortés Fulla on 01/12/10.
-//  Copyright 2010 Facultat d'Informatica de Barcelona (UPC). All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
-
-
-@interface LoginViewController : UIViewController {
-	
-@private
-	IBOutlet UITextField *nombre;
-	IBOutlet UITextField *contrasena;
-	NSMutableData *responseData;
-}
-
-@property (nonatomic, retain) IBOutlet UITextField *nombre;
-@property (nonatomic, retain) IBOutlet UITextField *contrasena;
-@property (nonatomic, retain) IBOutlet UITextField *inicia;
-
--(IBAction) iniciaSessio;
-
-#pragma mark -
-#pragma mark Delegat del NSURLConnection
-
-- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data;
-- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error;
-
-@end
+////  LoginViewController.h//  Expresso////  Created by David Cortés Fulla on 01/12/10.//  Copyright 2010 Facultat d'Informatica de Barcelona (UPC). All rights reserved.//#import <UIKit/UIKit.h>@interface LoginViewController : UIViewController {	@private	IBOutlet UITextField *nombre;	IBOutlet UITextField *contrasena;	NSMutableData *responseData;}@property (nonatomic, retain) IBOutlet UITextField *nombre;@property (nonatomic, retain) IBOutlet UITextField *contrasena;@property (nonatomic, retain) IBOutlet UITextField *inicia;-(IBAction) iniciaSessio;#pragma mark -#pragma mark Delegat del NSURLConnection- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data;- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error;@end
